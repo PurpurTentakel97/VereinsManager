95ff9d994173284ab264dc2c3ba83121ec1c7e98
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-02-08 16:26:34 +0100

    recreated database Class

--- main.py
+++ main.py
@@ -4,7 +4,8 @@
 import datetime
 from sqlite3 import OperationalError
 
-from logic import sqlite
+import sqlite
+from sqlite import handler
 
 from ui import base_window, main_window
 
@@ -17,32 +18,31 @@ def get_types(type_) -> list:
 
 
 def get_type_list(display_name: str) -> list:
-    return sqlite.database.get_type_list(
+    return sqlite.sql_database.database.types.get_type_list(
         table_name=sqlite.handler.get_type_from_display_name(display_name=display_name))
 
 
 def add_type(display_name: str, type_: str) -> None:
-    sqlite.database.add_type(table_name=sqlite.handler.get_type_from_display_name(display_name=display_name),
-                             type_=type_)
+    sqlite.sql_database.database.types.types.add_type(
+        table_name=sqlite.handler.get_type_from_display_name(display_name=display_name),
+        type_=type_)
 
 
 def edit_type(display_name: str, new_type_: str, type_id: int) -> None:
-    sqlite.database.edit_type(table_name=sqlite.handler.get_type_from_display_name(display_name=display_name),
-                              new_type=new_type_, type_id=type_id)
+    sqlite.sql_database.database.types.types.edit_type(
+        table_name=sqlite.handler.get_type_from_display_name(display_name=display_name),
+        new_type=new_type_, type_id=type_id)
 
 
 def remove_type(display_name: str, type_id: int) -> None:
-    commit_: bool = sqlite.database.remove_type(
+    sqlite.sql_database.database.types.types.remove_type(
         table_name=sqlite.handler.get_type_from_display_name(display_name=display_name),
         type_id=type_id)
-
-    if not commit_:
-        main_window.main_window_.set_status_bar("Type konnte nichtgelöscht werden.")
 
 
 # member
 def save_member(output: dict) -> int:
-    id_ = sqlite.database.save_member(output=output)
+    id_ = sqlite.sql_database.database.member.save_member(output=output)
 
     _log_initial_member_data(output=output, member_id=id_)
 
@@ -50,26 +50,27 @@ def save_member(output: dict) -> int:
 
 
 def update_member(output: dict) -> None:
-    reference_data: list = sqlite.database.load_all_data_from_member(id_=output[MemberTypes.ID.value])
+    reference_data: list = sqlite.sql_database.database.member.load_all_data_from_member(
+        id_=output[MemberTypes.ID.value])
     _log_update_member_data(output=output, reference_data=reference_data)
-    sqlite.database.update_member(output=output)
+    sqlite.sql_database.database.member.update_member(output=output)
 
 
 def delete_recover_member(output: list) -> None:
     member_id, active = output
-    reverence_data = sqlite.database.load_all_data_from_member(id_=member_id)
+    reverence_data = sqlite.sql_database.database.member.load_all_data_from_member(id_=member_id)
     reverence_data = [reverence_data[0], reverence_data[-1]]
-    sqlite.database.delete_recover_member(member_id=member_id, active=active)
+    sqlite.sql_database.database.member.delete_recover_member(member_id=member_id, active=active)
     _log_update_member_active(output=output, reverence_data=reverence_data)
 
 
 def load_all_member_names(active: bool) -> list:
-    return sqlite.database.load_all_member_names(active=active)
+    return sqlite.sql_database.database.member.load_all_member_names(active=active)
 
 
 def load_data_from_single_member(id_: int) -> list:
     try:
-        return sqlite.database.load_data_from_single_member(id_=id_)
+        return sqlite.sql_database.database.member.load_data_from_single_member(id_=id_)
     except OperationalError:
         return list()
 
@@ -93,8 +94,8 @@ def _log_initial_member_data(output: dict, member_id: int) -> None:
             case "":
                 continue
 
-        sqlite.database.log_data(member_id=member_id, log_type=log_type,
-                                 log_date=datetime.date.today(), old_data=None, new_data=value)
+        sqlite.sql_database.database.log.log_data(member_id=member_id, log_type=log_type,
+                                                  log_date=datetime.date.today(), old_data=None, new_data=value)
 
 
 def _log_update_member_data(output: dict, reference_data: list) -> None:
@@ -109,35 +110,37 @@ def _log_update_member_data(output: dict, reference_data: list) -> None:
             value = None
 
         if old_value != value:
-            sqlite.database.log_data(member_id=output[MemberTypes.ID.value], log_type=log_type,
-                                     log_date=datetime.date.today(), old_data=old_value, new_data=value)
+            sqlite.sql_database.database.log.log_data(member_id=output[MemberTypes.ID.value], log_type=log_type,
+                                                      log_date=datetime.date.today(), old_data=old_value,
+                                                      new_data=value)
 
 
 def _log_update_member_active(output: list, reverence_data: list) -> None:
     output_id, output_value = output
     reverence_data_id, reverence_data_value = reverence_data
     if output_value != reverence_data_value:
-        sqlite.database.log_data(member_id=reverence_data_id, log_type=MemberTypes.ACTIVE_MEMBER.value,
-                                 log_date=datetime.date.today(), old_data=reverence_data_value, new_data=output_value)
+        sqlite.sql_database.database.log.log_data(member_id=reverence_data_id, log_type=MemberTypes.ACTIVE_MEMBER.value,
+                                                  log_date=datetime.date.today(), old_data=reverence_data_value,
+                                                  new_data=output_value)
 
 
 def log_initial_member_nexus(member_id, log_type, new_data) -> None:
-    sqlite.database.log_data(member_id=member_id, log_type=log_type,
-                             log_date=datetime.date.today(), old_data=None, new_data=new_data)
+    sqlite.sql_database.database.log.log_data(member_id=member_id, log_type=log_type,
+                                              log_date=datetime.date.today(), old_data=None, new_data=new_data)
 
 
 def log_update_member_nexus(reverence_data: list, log_type: str, new_data) -> None:
     id_, member_id, type_id, value = reverence_data
     if value != new_data:
-        sqlite.database.log_data(member_id=member_id, log_type=log_type, log_date=datetime.date.today(), old_data=value,
-                                 new_data=new_data)
+        sqlite.sql_database.database.log.log_data(member_id=member_id, log_type=log_type,
+                                                  log_date=datetime.date.today(), old_data=value,
+                                                  new_data=new_data)
 
 
 # main
 if __name__ == "__main__":
-    sqlite.database = sqlite.Database()
-    sqlite.handler = sqlite.Handler()
-    sqlite.handler.create_all_tables()
+    handler.create_database_classes()
+    handler.create_all_tables()
     base_window.create_application()
     main_window.main_window_ = main_window.MainWindow()
     base_window.run_application()
---
+++ sqlite/sql_database.py
@@ -0,0 +1,48 @@
+# Purpur Tentakel
+# 21.01.2022
+# VereinsManager / SQLite
+
+import sqlite3
+
+from sqlite.sql_log import DatabaseLog
+from sqlite.sql_member import DatabaseMember
+from sqlite.sql_member_nexus import DatabaseMemberNexus
+from sqlite.sql_types import DatabaseTypes
+
+database: "Database" or None = None
+
+
+class Database:
+    def __init__(self) -> None:
+        self.connection = sqlite3.connect("saves/test.vm", detect_types=sqlite3.PARSE_DECLTYPES)  # TODO name save file
+        self.cursor = self.connection.cursor()
+
+        self.log = DatabaseLog(self)
+        self.member = DatabaseMember(self)
+        self.member_nexus = DatabaseMemberNexus(self)
+        self.types = DatabaseTypes(self)
+
+    def create(self, sql_command: str) -> None:
+        self.cursor.execute(sql_command)
+        self.connection.commit()
+
+    def insert(self, sql_command: str, values: list) -> int:
+        self.cursor.execute(sql_command, values)
+        self.connection.commit()
+        return self.cursor.lastrowid
+
+    def update(self, sql_command: str, values: list) -> None:
+        self.cursor.execute(sql_command, values)
+        self.connection.commit()
+
+    def delete(self, sql_command: str) -> None:
+        self.cursor.execute(sql_command)
+        self.connection.commit()
+
+    def select_all(self, sql_command: str) -> list:
+        self.cursor.execute(sql_command)
+        return self.cursor.fetchall()
+
+    def select_one(self, sql_command: str) -> list:
+        self.cursor.execute(sql_command)
+        return self.cursor.fetchone()
