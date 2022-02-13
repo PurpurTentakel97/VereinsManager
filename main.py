f01931a1b680dd57c753c95529a960fc3f926939
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-02-13 22:30:17 +0100

    reroll

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 # 21.01.2022
 # VereinsManager / Main
 
-from sqlite import global_handler as main_handler
+import config_sheet
+from sqlite import database
 
-from ui import base_window, main_window
 
 
 # main
 if __name__ == "__main__":
-    main_handler.create_database_classes()
-    main_handler.create_all_tables()
-    base_window.create_application()
-    main_window.main_window_ = main_window.MainWindow()
-    base_window.run_application()
+    database.crate_database()
+    #config_sheet.create_config()
+    #main_handler.create_database_classes()
+    #main_handler.create_all_tables()
+    #base_window.create_application()
+    #main_window.main_window_ = main_window.MainWindow()
+    #base_window.run_application()
