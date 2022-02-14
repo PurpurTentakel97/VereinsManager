fea98999d658c72de34233d1160df5fc9b75ab9b
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-02-14 23:54:06 +0100

    edit select member and type

--- main.py
+++ main.py
@@ -3,16 +3,23 @@
 # VereinsManager / Main
 
 import config_sheet
-from sqlite import database
+from sqlite import database, select_handler, global_handler
 
-
+from ui import base_window, main_window
 
 # main
 if __name__ == "__main__":
     database.crate_database()
     config_sheet.create_config()
-    #main_handler.create_database_classes()
-    #main_handler.create_all_tables()
-    #base_window.create_application()
-    #main_window.main_window_ = main_window.MainWindow()
-    #base_window.run_application()
+    global_handler.create_global_handler()
+    select_handler.select_handler.get_names_of_member()
+    select_handler.select_handler.get_names_of_member(active=False)
+    select_handler.select_handler.get_data_from_member_by_id(id_=1)
+    select_handler.select_handler.get_data_from_member_by_id(id_=3, active=False)
+    select_handler.select_handler.get_all_types()
+    select_handler.select_handler.get_all_types(active=False)
+    select_handler.select_handler.get_types_of_member()
+    select_handler.select_handler.get_types_of_member(active=False)
+    base_window.create_application()
+    main_window.main_window_ = main_window.MainWindow()
+    base_window.run_application()
--- transition.py
+++ transition.py
@@ -2,43 +2,12 @@
 # 08.02.2022
 # VereinsManager / Transition
 
-from sqlite import types_handler, global_handler, member_handler, member_nexus_handler
+from sqlite import global_handler, select_handler
+
+g_h = global_handler.global_handler
+s_h = select_handler.select_handler
 
 
-# config
-def get_display_types(type_) -> list:
-    return types_handler.get_display_types(type_type=type_)
-
-
-def get_type_list(display_name: str) -> list:
-    return types_handler.get_type_list(display_name=display_name)
-
-
-def add_type(display_name: str, type_: str) -> None:
-    types_handler.add_type(display_name=display_name, type_=type_)
-
-
-def edit_type(display_name: str, new_type_: str, type_id: int) -> None:
-    types_handler.edit_type(display_name=display_name, new_type_=new_type_, type_id=type_id)
-
-
-def remove_type(display_name: str, type_id: int) -> None:
-    types_handler.remove_type(display_name=display_name, type_id=type_id)
-
-
-# member
-def save_update_member(output: dict, time_stamp) -> dict:
-    return global_handler.save_update_member(output=output, time_stamp=time_stamp)
-
-
-def load_all_member_names(active: bool) -> list:
-    return member_handler.load_all_member_names(active=active)
-
-
-def load_data_single_member(id_: int) -> list:
-    return member_handler.load_data_from_single_member(id_=id_)
-
-
-# member nexus
-def load_member_nexus(member_id, table_type) -> list:
-    return member_nexus_handler.load_member_nexus(member_id=member_id, table_type=table_type)
+# type
+def get_types_of_member() -> tuple:
+    return s_h.get_types_of_member()
