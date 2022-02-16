ce30e3aa2020fd2a005a320691381c688c678ec6
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-02-17 00:10:23 +0100

    added/fixed raw_type in types_window.py

--- main.py
+++ main.py
@@ -3,23 +3,30 @@
 # VereinsManager / Main
 
 import config_sheet
-from sqlite import database, select_handler, global_handler
+from sqlite import database, global_handler as g_h, select_handler as s_h
 
 from ui import base_window, main_window
+
+
+def _test() -> None:
+    s_h.select_handler.get_raw_types()
+    # s_h.select_handler.get_names_of_member()
+    # s_h.select_handler.get_names_of_member(active=False)
+    # s_h.select_handler.get_data_from_member_by_id(id_=1)
+    # s_h.select_handler.get_data_from_member_by_id(id_=3, active=False)
+    # s_h.select_handler.get_all_types()
+    # s_h.select_handler.get_all_types(active=False)
+    # s_h.select_handler.get_types_of_member()
+    # s_h.select_handler.get_types_of_member(active=False)
+    pass
+
 
 # main
 if __name__ == "__main__":
     database.crate_database()
     config_sheet.create_config()
-    global_handler.create_global_handler()
-    select_handler.select_handler.get_names_of_member()
-    select_handler.select_handler.get_names_of_member(active=False)
-    select_handler.select_handler.get_data_from_member_by_id(id_=1)
-    select_handler.select_handler.get_data_from_member_by_id(id_=3, active=False)
-    select_handler.select_handler.get_all_types()
-    select_handler.select_handler.get_all_types(active=False)
-    select_handler.select_handler.get_types_of_member()
-    select_handler.select_handler.get_types_of_member(active=False)
+    g_h.create_global_handler()
+    _test()
     base_window.create_application()
     main_window.main_window_ = main_window.MainWindow()
     base_window.run_application()
