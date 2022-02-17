f96c2aa4077a77251f6d9090a6ad1be65c36f468
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-02-17 11:44:49 +0100

    added/fixed single_types in types_window.py

--- transition.py
+++ transition.py
@@ -8,3 +8,7 @@ from sqlite import global_handler as g_h, select_handler as s_h
 # type
 def get_raw_types() -> tuple:
     return s_h.select_handler.get_raw_types()
+
+
+def get_single_type(raw_type_id: int, active: bool = True) -> tuple:
+    return s_h.select_handler.get_single_type(raw_type_id=raw_type_id, active=active)
