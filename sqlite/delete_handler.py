bff993ffc546531f78c68a9d950ef702ae0925f8
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-03-04 15:44:10 +0100

    finished log type

--- sqlite/delete_handler.py
+++ sqlite/delete_handler.py
@@ -3,10 +3,12 @@
 # VereinsManager / Add Handler
 
 from sqlite.database import Database
+from sqlite import select_handler as s_h, log_handler as l_h
 from config import error_code as e
 from logic import validation as v
 import debug
-debug_str:str = "DeleteHandler"
+
+debug_str: str = "DeleteHandler"
 
 delete_handler: "DeleteHandler"
 
@@ -28,20 +30,22 @@ class DeleteHandler(Database):
 
         sql_command: str = """DELETE FROM type WHERE ID is ?;"""
         try:
+            name: str = s_h.select_handler.get_type_name_by_id(ID=id_)
             self.cursor.execute(sql_command, (id_,))
             self.connection.commit()
+            l_h.log_handler.log_type(target_id=id_, target_column="name", old_data=name[0], new_data=None)
             return
 
         except self.OperationalError as error:
             debug.error(item=debug_str, keyword="delete_type", message=f"delete type failed\n"
-                                                                  f"command = {sql_command}\n"
-                                                                  f"error = {' '.join(error.args)}")
+                                                                       f"command = {sql_command}\n"
+                                                                       f"error = {' '.join(error.args)}")
             return e.DeleteFailed(info="Typ").message
 
         except self.IntegrityError as error:
             debug.error(item=debug_str, keyword="delete_type", message=f"delete type still used\n"
-                                                                  f"command = {sql_command}\n"
-                                                                  f"error = {' '.join(error.args)}")
+                                                                       f"command = {sql_command}\n"
+                                                                       f"error = {' '.join(error.args)}")
             self.connection.commit()
             return e.ForeignKeyError(info="Typ").message
 
