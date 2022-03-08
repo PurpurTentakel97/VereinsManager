ebaac2b4837d0ca1da953265162d674161f28b68
martin <martin.schreurs@netcologne.de>
2022-03-08 08:58:54 +0100

    finished member pdf

--- pdf_handler/member_pdf.py
+++ pdf_handler/member_pdf.py
@@ -1,12 +1,13 @@
 # Purpur Tentakel
 # 06.03.2022
 # VereinsManager / PDF / Member Table
-
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import inch, A4
 from reportlab.platypus import Paragraph, Table, SimpleDocTemplate
 from reportlab.lib.styles import getSampleStyleSheet, StyleSheet1
+from reportlab.lib.units import cm
 
+from datetime import datetime
 import os
 
 from logic import table_data_handler
@@ -23,29 +24,60 @@ style_sheet: StyleSheet1
 
 def member_pdf(path: str, active: bool):
     _transform_path(path=path)
-
-    data = table_data_handler.get_member_table_data(active=active)
-
-    type_ids = s_h.select_handler.get_single_raw_type_types(c.config.raw_type_id["membership"])
-    type_ids = [[x[0], x[1]] for x in type_ids]
+    _create_dir(dir_name)
 
     global style_sheet
     style_sheet = getSampleStyleSheet()
-    doc = SimpleDocTemplate("test.pdf", pagesize=[A4[1], A4[0]])
+    doc = SimpleDocTemplate(f"{dir_name}/{file_name}", pagesize=[A4[1], A4[0]], rightMargin=2 * cm, leftMargin=2 * cm,
+                            topMargin=2 * cm, bottomMargin=2 * cm)
+    elements: list = [
+        Paragraph("Mitglieder", style_sheet["Title"])
+    ]
 
-    elements: list = list()
+    data = table_data_handler.get_member_table_data(active=active)
+    if isinstance(data, str):
+        return data
+    if not data:
+        elements.append(Paragraph(
+            f"Keine Mitglieder vorhanden // Stand: {datetime.strftime(datetime.now(), c.config.date_format['short'])}",
+            style_sheet["BodyText"]))
+        doc.build(elements)
+        return
+
+    type_ids = s_h.select_handler.get_single_raw_type_types(c.config.raw_type_id["membership"])
+    if isinstance(type_ids, str):
+        return type_ids
+    type_ids = [[x[0], x[1]] for x in type_ids]
+
     for type_id, type in type_ids:
         all_members: list = data[type_id]
+        elements.append(Paragraph(type, style_sheet["Heading3"]))
         if not all_members:
+            elements.append(Paragraph(
+                f"Keine Mitglieder vorhanden // Stand: {datetime.strftime(datetime.now(), c.config.date_format['short'])}",
+                style_sheet["BodyText"]))
             continue
-        table_data: list = [[type, "Name", "Adresse", "Alter", "Eintritt", "Telefon", "Mail"]]
-        style_data: list = []
+
+        table_data: list = [[
+            f"Stand: {datetime.strftime(datetime.now(), c.config.date_format['short'])}",
+            "Name",
+            "Adresse",
+            "Alter",
+            "Eintritt",
+            "Telefon",
+            "Mail",
+        ]]
+        style_data: list = [
+            ("GRID", (1, 1), (-1, -1), 1, colors.black),
+            ("GRID", (0, 0), (0, -1), 3, colors.black),
+            ("GRID", (0, 0), (-1, 0), 3, colors.black),
+        ]
         for index, member in enumerate(all_members, start=1):
             member_data = member["member"]
             phone_data = member["phone"]
             mail_data = member["mail"]
             row_data: list = [
-                str(index),
+                str(index) if not member_data[9] else f"{str(index)} (E)",
                 [_paragraph(member_data[0]), _paragraph(member_data[1])],
                 [_paragraph(member_data[2]), _paragraph(member_data[3]), _paragraph(member_data[4])],
                 [_paragraph(member_data[6]), _paragraph(member_data[5])],
@@ -62,9 +94,9 @@ def member_pdf(path: str, active: bool):
             if member_data[8] is not None and int(member_data[8]) % 5 == 0:
                 style_data.append(("BACKGROUND", (4, index), (4, index), colors.lightgrey))
 
-        t = Table(table_data, style=style_data)
-        t._argW[3] = 1.5 * inch
-        elements.append(t)
+        table = Table(table_data, style=style_data)
+        table._argW[3] = 1.5 * inch
+        elements.append(table)
     doc.build(elements)
 
 
@@ -83,3 +115,8 @@ def _paragraph(value) -> Paragraph:
         return Paragraph(str(value[0]) + ": " + str("---" if not value[1] else value[1]), style_sheet["BodyText"])
     else:
         return Paragraph(str("---" if not value else value), style_sheet["BodyText"])
+
+
+def _create_dir(dirs: str) -> None:
+    if not os.path.exists(dirs):
+        os.mkdir(dirs)
