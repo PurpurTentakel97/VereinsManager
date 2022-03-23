0a94244f67488b6a36d1490e1b4466420022f32d
PurpurTentakel97 <martin.schreurs@netcologne.de>
2022-03-23 13:42:12 +0100

    refactor delete_handler.py

--- sqlite/delete_handler.py
+++ sqlite/delete_handler.py
@@ -26,25 +26,14 @@ class DeleteHandler(Database):
             name: str = s_h.select_handler.get_type_name_by_ID(ID=ID)
             self.cursor.execute(sql_command, (ID,))
             self.connection.commit()
-            result, valid = l_h.log_handler.log_type(target_id=ID, target_column="name", old_data=name[0],
-                                                     new_data=None)
-            if not valid:
-                return result, False
+            l_h.log_handler.log_type(target_id=ID, target_column="name", old_data=name[0],
+                                     new_data=None)
+        except self.OperationalError:
+            raise e.DeleteFailed(info="Typ")
 
-            return None, True
-
-        except self.OperationalError as error:
-            debug.error(item=debug_str, keyword="delete_type", message=f"delete type failed\n"
-                                                                       f"command = {sql_command}\n"
-                                                                       f"error = {' '.join(error.args)}")
-            return e.DeleteFailed(info="Typ").message, False
-
-        except self.IntegrityError as error:
-            debug.error(item=debug_str, keyword="delete_type", message=f"delete type still used\n"
-                                                                       f"command = {sql_command}\n"
-                                                                       f"error = {' '.join(error.args)}")
+        except self.IntegrityError:
             self.connection.commit()
-            return e.ForeignKeyError(info="Typ").message, False
+            raise e.ForeignKeyError(info="Typ")
 
 
 def create_delete_handler() -> None:
