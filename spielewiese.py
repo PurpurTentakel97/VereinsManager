96f8c39f74151d4e745e077f5b3cdf6e0239f6cc
martin <martin.schreurs@netcologne.de>
2022-04-07 13:34:10 +0200

    tried letter

--- spielewiese.py
+++ spielewiese.py
@@ -1,12 +1,13 @@
-import random
+from config import config_sheet as c
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.styles import ParagraphStyle
 from reportlab.lib.units import cm
 from reportlab.platypus import (
-    BaseDocTemplate, PageTemplate, PageBreak, Frame, FrameBreak, Paragraph,
+    BaseDocTemplate, PageTemplate, Frame, FrameBreak, Paragraph, Spacer, Image,
 )
 
 if __name__ == "__main__":
+    c.create_config()
     style_1 = ParagraphStyle(
         name='Stylo',
         fontName='Helvetica',
@@ -23,39 +24,44 @@ if __name__ == "__main__":
 
     frameWidth = 4 * cm
     frameHeight = doc.height - 0.05 * cm
+    footerHeight = 1 * cm
 
     frame_list = [
         Frame(
             x1=doc.leftMargin,
-            y1=doc.bottomMargin,
+            y1=doc.bottomMargin + footerHeight,
             width=doc.width - frameWidth,
-            height=frameHeight),
+            height=frameHeight - footerHeight),
         Frame(
             x1=doc.leftMargin + doc.width - frameWidth,
-            y1=doc.bottomMargin,
+            y1=doc.bottomMargin + footerHeight,
             width=frameWidth,
-            height=frameHeight),
+            height=frameHeight - footerHeight),
+        Frame(
+            x1=doc.leftMargin,
+            y1=doc.bottomMargin,
+            width=doc.width,
+            height=footerHeight),
     ]
-    doc.addPageTemplates([PageTemplate(id='frames', frames=frame_list), ])
+    doc.addPageTemplates([PageTemplate(id='frames', frames=frame_list)])
 
-    story = []
-    for i, x in enumerate(['A', 'B', 'C']):
-        # add text in first frame
-        for _ in range(3):
-            story.append(
-                Paragraph(
-                    x * random.randint(50, 100),
-                    style_1))
+    story = [Paragraph("Name // Straße // plz / stadt // nummber // mail", style=style_1),
+             Paragraph("Name Empfänger<br/>Straße Enpfänger<br/>plz / stadt Empfänger", style=style_1),
+             Spacer(0, 1 * cm), Paragraph("Datum", style=style_1), Spacer(0, 0.5 * cm),
+             Paragraph("Eintrittsbestätigung", style=style_1), Spacer(0, 1 * cm),
+             Paragraph("<<Name>> wurde am <<Datum>> in den <<Vereinsname>> aufgenommen.", style_1), Spacer(0, 1 * cm),
+             Paragraph("Informationen:", style_1), Spacer(0, 0.1 * cm),
+             Paragraph("Mitgliedsart:<<mitgliedsart>><br/>Jahresbeitrag:<<beitrag>>", style_1), Spacer(0, 1.5 * cm),
+             Paragraph("________________________", style_1), Paragraph("Vorstandsmitglied", style_1),
+             # next frame
+             FrameBreak(),
+             Image(c.config.get_icon_path(), frameWidth, frameWidth), Spacer(0, 0.5 * cm), Paragraph(
+            "Vorstand name<br/>Vorstand Adresse<br/>Vorstand plz / Stadt<br/>Vorstand mail<br/>Vorstand nummer<br/>homepage"),
+             Spacer(0, 0.5 * cm),
+             Paragraph("Sondertext für den Briefkopf<br/>* Deutscher Meitser 1987 *<br/>* Bester Coder *"),
+             # next frame
+             FrameBreak(),
+             Paragraph("Bankverbindung", style_1)]
 
-        # jump to next frame
-        story.append(FrameBreak())
-
-        # add text in second frame
-        story.append(
-            Paragraph(
-                'This should be on the top of the 2nd Frame! ' + x,
-                style_1))
-
-        story.append(PageBreak())
 
     doc.build(story)
